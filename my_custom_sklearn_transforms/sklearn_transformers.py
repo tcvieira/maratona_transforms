d5f82de8c9e80c6a56eb169d12ed04568275f730
tcvieira <thiagotcvieira@gmail.com>
2020-09-01 01:35:58 -0300

    new transformer

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -28,3 +28,29 @@ class MyTranformer(BaseEstimator, TransformerMixin):
         data.drop(labels=['NOME', 'MATRICULA', 'NOTA_GO'], axis='columns', inplace=True)
         # Retornamos um novo dataframe
         return data
+
+
+from sklearn.preprocessing import LabelEncoder
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class MasterTransformer2(BaseEstimator, TransformerMixin):
+    def __init__(self):        
+        pass
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        columns = ['Estilo de vida', 'Beneficios', 'Satisfação com a relação', 'Performance na entrevista',
+            'Necessita de hora extra', 'Satisfação com emprego', 'Posicao', 'Envolvimento com trabalho', 
+            'Satisfação com o ambiente no emprego atual', 'Genero','Cargo', 'Estado civil',
+            'Local de trabalho', 'Educacao', 'Area', 'Anos com a mesma gerência', 
+            'Anos desde última promoção', 'Anos na posição atual', 'Anos na última empresa', 
+            'Horas de treinamento ultimo ano', 'Anos de experiencia', 'Aumento de salario%', 
+            'Quantidade de empresas que trabalho', 'Bonus de performance', 'Renda', 'Horas voluntariado',
+            'Idade', 'Pontuação teste', 'Distancia casa-trabalho', 'Subordinado']
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        for col in columns:
+            data[col] = LabelEncoder().fit_transform(data[col])
+        return data
